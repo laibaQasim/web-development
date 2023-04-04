f1967829da3cddd6072898b2bb9f2598ce9f6229
laibaQasim <126785296+laibaQasim@users.noreply.github.com>
2023-04-04 16:30:24 +0500

    Lecture 03
    Read and write data from file. (Streams)

---
+++ lectures.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+namespace HW_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //READ DATA FROM ONE FILE AND WRITE TO OTHER FILE
+            FileStream fout = new FileStream("file1.txt", FileMode.Open, FileAccess.Read);
+            FileStream fin = new FileStream("file2.txt", FileMode.Create, FileAccess.Write);
+            int i = fout.ReadByte();
+            char c = (char)i;
+            if (i == -1)
+                return;
+
+            while (i != -1)
+            {
+                c = (char)i;
+                fin.WriteByte((byte)c);
+                i = fout.ReadByte();
+                Console.WriteLine(c);
+
+            }
+            fin.Close();
+            fout.Close();
+
+            //CODE FOR READING FROM FILE
+            //for (char ch = 'A'; ch <= 'Z'; ch++)
+            //    fout.WriteByte((byte)ch);
+            //fout.Close();
+
+        }
+    }
+}
