6a11dc4ee031b262e6bae1c8a4e9ec3a0032b216
laibaQasim <126785296+laibaQasim@users.noreply.github.com>
2023-04-18 19:27:56 +0500

    Lecture 08
    First application having simple student form

---
+++ lecture 08/Controllers/StudentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyApplication.Controllers
+{
+    public class StudentController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+        public ViewResult StudentForm()
+        {
+            return View();
+        }
+    }
+}
---
+++ lecture 08/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace MyApplication.Models
+{
+    public class Student
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CGPA { get; set; }
+        public string Semester { get; set;}
+
+    }
+}
